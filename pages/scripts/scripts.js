bd20e154cc45d70623ff946d3259861e0184a7eb
Martinhache475 <108502607+Martinhache475@users.noreply.github.com>
2022-10-04 10:22:56 -0300

    buttons and languages
    agrego botones, y links de idiomas

---
+++ pages/scripts/scripts.js
@@ -0,0 +1,11 @@
+function calcular(){
+    var puntos = Number( document.getElementById("puntos").innerHTML);
+    var suma = puntos + 1;
+    document.getElementById("puntos").innerHTML = suma;
+}
+
+function Calculo(){
+    document.getElementById("puntos").innerHTML = suma
+    if (suma >=10) {alert ("genio") }
+}
+
